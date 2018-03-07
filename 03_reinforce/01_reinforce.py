8847845f6f8bf7d9450433c44e5e45069074f5fd
fanrong33 <fanrong33@qq.com>
2018-03-08 00:32:53 +0900

    update

--- 03_reinforce/01_reinforce.py
+++ 03_reinforce/01_reinforce.py
@@ -1,7 +1,14 @@
 # encoding: utf-8
 """
 
+强化学习是一个通过奖惩来学习正确行为的机制. 家族中有很多种不一样的成员, 有学习奖惩值, 根据自己认为的高价值选行为,
+比如 Q learning, Deep Q Network, 也有不通过分析奖励值, 直接输出行为的方法, 这就是今天要说的 Policy Gradients 了.
+甚至我们可以为 Policy Gradients 加上一个神经网络来输出预测的动作. 对比起以值为基础的方法, Policy Gradients
+直接输出动作的最大好处就是, 它能在一个连续区间内挑选动作, 而基于值的, 比如 Q-learning, 它如果在无穷多的动作中计算价值,
+从而选择行为, 这, 它可吃不消.
+
 https://github.com/pytorch/examples/blob/master/reinforcement_learning/reinforce.py
+https://morvanzhou.github.io/tutorials/machine-learning/reinforcement-learning/5-1-A-PG/
 
 """
 import argparse
@@ -27,9 +34,12 @@ parser.add_argument('--render', action='store_true',
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='interval between training status logs (default: 10)')
 args = parser.parse_args()
+# print(args)
+''' Namespace(gamma=0.99, log_interval=10, render=False, seed=543) '''
 
 
 env = gym.make('CartPole-v0')
+env = env.unwrapped
 env.seed(args.seed)
 torch.manual_seed(args.seed)
 
@@ -53,16 +63,18 @@ policy = Policy()
 optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 
 
-def select_action(state):
+def choose_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
+    # 让 Policy 神经网络生成所有 action 的值, 并选择值最大的 action
     probs = policy(Variable(state))
+
     m = Categorical(probs)
     action = m.sample()
     policy.saved_log_probs.append(m.log_prob(action))
     return action.data[0]
 
 
-def finish_episode():
+def learn():
     R = 0
     policy_loss = []
     rewards = []
@@ -73,29 +85,44 @@ def finish_episode():
     rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
     for log_prob, reward in zip(policy.saved_log_probs, rewards):
         policy_loss.append(-log_prob * reward)
-    optimizer.zero_grad()
     policy_loss = torch.cat(policy_loss).sum()
+
+    # 计算，更新神经网络参数
+    optimizer.zero_grad()
     policy_loss.backward()
     optimizer.step()
     del policy.rewards[:]
     del policy.saved_log_probs[:]
 
 
+
 def main():
     running_reward = 10
     for i_episode in count(1):
+        # 初始化环境
         state = env.reset()
         for t in range(10000):  # Don't infinite loop while learning
-            action = select_action(state)
-            state, reward, done, _ = env.step(action)
-            if args.render:
-                env.render()
+        # while True:
+            # 刷新环境
+            # if args.render:
+            #     env.render()
+            # env.render()
+
+            # Policy 选择行为
+            action = choose_action(state)
+
+            # 环境根据行为给出下一个 state, reward, 是否终止
+            state_, reward, done, info = env.step(action)
+
             policy.rewards.append(reward)
+
+            state = state_
+
             if done:
                 break
-
+        
         running_reward = running_reward * 0.99 + t * 0.01
-        finish_episode()
+        learn()
         if i_episode % args.log_interval == 0:
             print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
                 i_episode, t, running_reward))
